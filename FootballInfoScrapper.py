e01bcaade922d3795b7aba87f63245836808a103
JosephLavoie <154958953+JosephLavoie@users.noreply.github.com>
2023-12-27 07:07:38 +0000

    Ability to generate all weeks urls, took out years 2021-22, due to extra week (for now)

--- FootballInfoScrapper.py
+++ FootballInfoScrapper.py
@@ -1,4 +1,4 @@
-def generate_urls(start, end):
+def generate_year_urls(start, end):
     url_list = []
     
     for year in range(start, end + 1):
@@ -7,9 +7,26 @@ def generate_urls(start, end):
     
     return url_list
 
+def generate_week_urls(year_url_list, start, end):
+    url_list = []
+    
+    for year in year_url_list:
+        for week in range(start, end + 1):
+            url = f"{year}week_{week}.htm"
+            url_list.append(url)
+    
+    return url_list
+
 def main():
     # Generate list of years
-    urls = generate_urls(2000, 2022)
+    yearUrls = generate_year_urls(2000, 2020)
+    weekUrls = generate_week_urls(yearUrls, 1, 21)
+
+    print(weekUrls)
+
+
+
+    print(weekUrls)
 
 if __name__ == "__main__":
     main()
