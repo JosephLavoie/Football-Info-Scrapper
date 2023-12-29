35cdb6b28049bb959a4f8b2f4104900e79e1f8d0
JosephLavoie <154958953+JosephLavoie@users.noreply.github.com>
2023-12-29 02:32:11 +0000

    Program to format dates to CSV works!

--- DateToCSVFormat.py
+++ DateToCSVFormat.py
@@ -8,6 +8,7 @@ print("Current Working Directory:", os.getcwd())
 def main():
     
     unformatted_dates = []
+    formatted_dates = []
 
     with open('unformatted_dates.txt', 'r') as file:
         # Read each line from the file and append it to the list
@@ -15,16 +16,48 @@ def main():
             unformatted_dates.append(line.strip())
     
     for date in unformatted_dates:
-        year = date[:4]
-        month = date[4:6]
-        day = date[6:]
+        year = int(date[:4])
+        month = int(date[4:6])
+        day = int(date[6:])
         print(f"Year: {year}, Month: {month}, Day: {day}")
 
+        relative_year = (year - 2000)
+
+        month_addon = 0
+        if month == 10:
+            month_addon = 31
+            month_name = "October"
+        elif month == 11:
+            month_addon = 61
+            month_name = "November"
+        elif month == 12:
+            month_addon = 92
+            month_name = "December"
+        elif month == 1:
+            month_addon = 123-365
+            month_name = "January"
+        elif month == 2:
+            month_addon = 151-365
+            month_name = "February"
+        else:
+            month_name = "September"
+        
+        relative_days_in_year = day - 3 + month_addon
+
+        total_days = (relative_year*365) + relative_days_in_year
+
+        # For adding the extra day due to leap years
+        total_days += (relative_year // 4)
+
+        formatted_dates.append([year,month_name,total_days])
+
+
     
-    #with open('bad_game_url_list.txt', 'w') as file:
+    with open('datesCSV.txt', 'w') as file:
         # Convert each element of the list to a string and write it to the file
-        #for item in gameUrls[1]:
-            #file.write(str(item) + '\n')
+        file.write('"Year","Month","Days Since Sept 3rd 2000"')
+        for item in formatted_dates:
+            file.write(f"{str(item[0])},{str(item[1])},{str(item[2])} \n")
 
 if __name__ == "__main__":
     main()
