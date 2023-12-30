c9b7d4cd073bc770dd3409db7ae1c0e3a108a9ca
JosephLavoie <154958953+JosephLavoie@users.noreply.github.com>
2023-12-30 00:14:39 +0000

    Created the total method of websrcaping in Information_Getter()

--- FootballGameInfoScraper.py
+++ FootballGameInfoScraper.py
@@ -6,23 +6,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+options.add_argument("disable-quic")
 
 # Set up the WebDriver (for example, using Chrome)
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 
 def Information_Getter(game_url_list):
+
     infoCSV = []
     bad_infoCSV = []
     line = 0
 
-    for url in game_url_list:
+    for url in game_url_list[:10]:
 
-        time.sleep(5)
         response = requests.get(url)
 
         wait_time = response.headers.get('Retry-After')
-
         if wait_time:
             print(wait_time)
             time.sleep(int(wait_time) + 1)
@@ -45,31 +50,95 @@ def Information_Getter(game_url_list):
                 statium = "N/A"
 
             #---------------------------------------------------------------------------------------
+            #---------------------------------------------------------------------------------------
+            #Selenium
+            #---------------------------------------------------------------------------------------
+            #---------------------------------------------------------------------------------------
+                
+            driver.get(url)
+            # Introduce a delay (e.g., 4 seconds) to allow dynamic content to load
+            driver.implicitly_wait(4)
+
+            #---------------------------------------------------------------------------------------
             # Roof
             #---------------------------------------------------------------------------------------
-            
+            try:    
+                roof_element = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, '//th[@data-stat="info" and text()="Roof"]'))
+                )
+
+                roof = roof_element.find_element(By.XPATH, '../td[@data-stat="stat"]').text
+
+            except:
+                print("Missing: Roof")
+                roof = "N/A"
             #---------------------------------------------------------------------------------------
             # Surface
             #---------------------------------------------------------------------------------------
+            try:
+                surface_element = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, '//th[@data-stat="info" and text()="Surface"]'))
+                )
+
+                surface = surface_element.find_element(By.XPATH, '../td[@data-stat="stat"]').text
+
+            except:
+                print("Missing: Surface")
+                surface = "N/A"
 
             #---------------------------------------------------------------------------------------
             # Field Goals
             #---------------------------------------------------------------------------------------
 
+            try:
+                fgm_elements = WebDriverWait(driver, 10).until(
+                    EC.presence_of_all_elements_located((By.XPATH, '//td[@data-stat="fgm"]'))
+                )
+
+                fga_elements = WebDriverWait(driver, 10).until(
+                    EC.presence_of_all_elements_located((By.XPATH, '//td[@data-stat="fga"]'))
+                )
+
+                fgm, fga = 0,0
+                for fgm_element, fga_element in zip(fgm_elements, fga_elements):
+                    fgm_per = fgm_element.text
+                    fga_per = fga_element.text
+
+                    if fgm_per:
+                        fgm += int(fgm_per)
+                    if fga_per:
+                        fga += int(fga_per)
+            except:
+                fgm, fga = 0,0
+
             #---------------------------------------------------------------------------------------
             # Weather
             #---------------------------------------------------------------------------------------
-            
-            
-            infoCSV.append(statium + "," + roof + "," + surface + "," + fga + "," + fgm + "," + weather)
+            try:
+                weather_element = WebDriverWait(driver, 5).until(
+                    EC.presence_of_element_located((By.XPATH, '//th[@data-stat="info" and text()="Weather"]'))
+                )
+
+                weather = weather_element.find_element(By.XPATH, '../td[@data-stat="stat"]').text
+
+            except:
+                print("Missing: Weather")
+                weather = "N/A"
+
+            #---------------------------------------------------------------------------------------
+            #---------------------------------------------------------------------------------------
+            #---------------------------------------------------------------------------------------
+
+            infoCSV.append(str(line) + "," + statium + "," + roof + "," + surface + "," + str(fgm) + "," + str(fga) + "," + weather)
+            print("#" + str(line) + "," + statium + "," + roof + "," + surface + "," + str(fgm) + "," + str(fga) + "," + weather)
+
         else:
             print(f"Failed to retrieve the HTML content. Status code: {response.status_code}")
-            bad_infoCSV.append(url + "," + str(line))
+            bad_infoCSV.append(str(line) + "," + url)
 
 
         print(f"#{line} out of ~8000")
         line += 1
-
 
     return [infoCSV, bad_infoCSV]
 
